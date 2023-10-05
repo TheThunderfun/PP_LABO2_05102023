349d19739cd39c01023644626678b6218993190c
FranciscoH27 <franuleg@gmail.com>
2023-10-05 02:09:35 -0300

    Agregar archivos de proyecto.

---
+++ FormLucas/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace CalculadoraForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbOperacion = new ComboBox();
+            txtPrimerOperador = new TextBox();
+            txtSegundoOperador = new TextBox();
+            btnOperar = new Button();
+            btnLimpiar = new Button();
+            btnCerrar = new Button();
+            grpSistema = new GroupBox();
+            rdbBinario = new RadioButton();
+            rdbDecimal = new RadioButton();
+            lblOperacion = new Label();
+            lblPrimerOperador = new Label();
+            lblSegundoOperador = new Label();
+            lblResultado = new Label();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            grpSistema.SuspendLayout();
+            SuspendLayout();
+            // 
+            // cmbOperacion
+            // 
+            cmbOperacion.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbOperacion.FormattingEnabled = true;
+            cmbOperacion.Location = new Point(308, 286);
+            cmbOperacion.Name = "cmbOperacion";
+            cmbOperacion.Size = new Size(156, 23);
+            cmbOperacion.TabIndex = 0;
+            cmbOperacion.SelectedIndexChanged += cmbOperacion_SelectedIndexChanged;
+            // 
+            // txtPrimerOperador
+            // 
+            txtPrimerOperador.Location = new Point(84, 286);
+            txtPrimerOperador.Name = "txtPrimerOperador";
+            txtPrimerOperador.Size = new Size(156, 23);
+            txtPrimerOperador.TabIndex = 1;
+            txtPrimerOperador.TextChanged += txtPrimerOperador_TextChanged;
+            // 
+            // txtSegundoOperador
+            // 
+            txtSegundoOperador.Location = new Point(553, 286);
+            txtSegundoOperador.Name = "txtSegundoOperador";
+            txtSegundoOperador.Size = new Size(156, 23);
+            txtSegundoOperador.TabIndex = 2;
+            txtSegundoOperador.TextChanged += txtSegundoOperador_TextChanged;
+            // 
+            // btnOperar
+            // 
+            btnOperar.Location = new Point(84, 356);
+            btnOperar.Name = "btnOperar";
+            btnOperar.Size = new Size(156, 33);
+            btnOperar.TabIndex = 3;
+            btnOperar.Text = "Operar";
+            btnOperar.UseVisualStyleBackColor = true;
+            btnOperar.Click += btnOperar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(308, 356);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(156, 33);
+            btnLimpiar.TabIndex = 4;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Location = new Point(553, 356);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(156, 33);
+            btnCerrar.TabIndex = 5;
+            btnCerrar.Text = "Cerrar";
+            btnCerrar.UseVisualStyleBackColor = true;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // grpSistema
+            // 
+            grpSistema.Controls.Add(rdbBinario);
+            grpSistema.Controls.Add(rdbDecimal);
+            grpSistema.Location = new Point(290, 134);
+            grpSistema.Name = "grpSistema";
+            grpSistema.Size = new Size(187, 67);
+            grpSistema.TabIndex = 6;
+            grpSistema.TabStop = false;
+            grpSistema.Text = "Representar resultado en:";
+            // 
+            // rdbBinario
+            // 
+            rdbBinario.AutoSize = true;
+            rdbBinario.Location = new Point(111, 33);
+            rdbBinario.Name = "rdbBinario";
+            rdbBinario.Size = new Size(62, 19);
+            rdbBinario.TabIndex = 1;
+            rdbBinario.Text = "Binario";
+            rdbBinario.UseVisualStyleBackColor = true;
+            rdbBinario.CheckedChanged += rdbBinario_CheckedChanged;
+            // 
+            // rdbDecimal
+            // 
+            rdbDecimal.AutoSize = true;
+            rdbDecimal.Checked = true;
+            rdbDecimal.Location = new Point(6, 33);
+            rdbDecimal.Name = "rdbDecimal";
+            rdbDecimal.Size = new Size(68, 19);
+            rdbDecimal.TabIndex = 0;
+            rdbDecimal.TabStop = true;
+            rdbDecimal.Text = "Decimal";
+            rdbDecimal.UseVisualStyleBackColor = true;
+            rdbDecimal.CheckedChanged += rdbDecimal_CheckedChanged;
+            // 
+            // lblOperacion
+            // 
+            lblOperacion.AutoSize = true;
+            lblOperacion.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblOperacion.Location = new Point(335, 249);
+            lblOperacion.Name = "lblOperacion";
+            lblOperacion.Size = new Size(109, 25);
+            lblOperacion.TabIndex = 3;
+            lblOperacion.Text = "Operacion:";
+            // 
+            // lblPrimerOperador
+            // 
+            lblPrimerOperador.AutoSize = true;
+            lblPrimerOperador.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPrimerOperador.Location = new Point(84, 249);
+            lblPrimerOperador.Name = "lblPrimerOperador";
+            lblPrimerOperador.Size = new Size(167, 25);
+            lblPrimerOperador.TabIndex = 2;
+            lblPrimerOperador.Text = "Primer operador:";
+            // 
+            // lblSegundoOperador
+            // 
+            lblSegundoOperador.AutoSize = true;
+            lblSegundoOperador.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSegundoOperador.Location = new Point(540, 249);
+            lblSegundoOperador.Name = "lblSegundoOperador";
+            lblSegundoOperador.Size = new Size(188, 25);
+            lblSegundoOperador.TabIndex = 4;
+            lblSegundoOperador.Text = "Segundo operador:";
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
+            lblResultado.Location = new Point(84, 33);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(152, 37);
+            lblResultado.TabIndex = 7;
+            lblResultado.Text = "Resultado:";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(553, 58);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(164, 169);
+            listBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(593, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 25);
+            label1.TabIndex = 9;
+            label1.Text = "Historial";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(765, 477);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Controls.Add(lblResultado);
+            Controls.Add(lblPrimerOperador);
+            Controls.Add(lblOperacion);
+            Controls.Add(lblSegundoOperador);
+            Controls.Add(grpSistema);
+            Controls.Add(btnCerrar);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnOperar);
+            Controls.Add(txtSegundoOperador);
+            Controls.Add(txtPrimerOperador);
+            Controls.Add(cmbOperacion);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculadora Alumno: Francisco Hulej";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            grpSistema.ResumeLayout(false);
+            grpSistema.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbOperacion;
+        private TextBox txtPrimerOperador;
+        private TextBox txtSegundoOperador;
+        private Button btnOperar;
+        private Button btnLimpiar;
+        private Button btnCerrar;
+        private GroupBox grpSistema;
+        private RadioButton rdbBinario;
+        private RadioButton rdbDecimal;
+        private Label lblOperacion;
+        private Label lblPrimerOperador;
+        private Label lblSegundoOperador;
+        private Label lblResultado;
+        private ListBox listBox1;
+        private Label label1;
+    }
+}
